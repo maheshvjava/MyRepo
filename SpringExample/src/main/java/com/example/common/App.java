7f90f90d1f4fb98d69977a257ddfa1150bf8692e
MVeeranala <Maheswara.Veeranala@altimetrik.com>
2016-08-30 15:15:56 +0530

    added commit

--- SpringExample/src/main/java/com/example/common/App.java
+++ SpringExample/src/main/java/com/example/common/App.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ * Simple Hello world!
  * Spring Maven Example.
  */
 public class App 
